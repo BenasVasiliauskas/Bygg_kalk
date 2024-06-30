07335d8def9a34b7160849f8bc75ce90dde61beb
Benas Vasiliauskas <benasvasi@gmail.com>
2024-06-30 12:04:49 +0300

    moved calc quantity outside terrace screen

--- lib/pages/item_screen/terrace_item_screen.dart
+++ lib/pages/item_screen/terrace_item_screen.dart
@@ -38,6 +38,8 @@ class TerraceItemScreen extends StatefulWidget {
   State<TerraceItemScreen> createState() => _TerraceItemScreenState();
 }
 
+TextEditingController terraceCalculationControllers = TextEditingController();
+double calculationQuantity = 0;
 List<double> emptyCustomList = [];
 
 class _TerraceItemScreenState extends State<TerraceItemScreen> {
@@ -54,8 +56,6 @@ class _TerraceItemScreenState extends State<TerraceItemScreen> {
   late TextEditingController savingController;
   late TextEditingController loadingController;
 //
-  TextEditingController quantityCalculationControllers =
-      TextEditingController();
 
   //
 
@@ -168,6 +168,8 @@ class _TerraceItemScreenState extends State<TerraceItemScreen> {
     initialiseEmptyList();
     savingController = TextEditingController();
     loadingController = TextEditingController();
+    if (terraceCalculationControllers.text != "")
+      calculationQuantity = double.parse(terraceCalculationControllers.text);
   }
 
   void calculateCalculationQuantity() {
@@ -178,8 +180,7 @@ class _TerraceItemScreenState extends State<TerraceItemScreen> {
 
     calculationQuantity = mat2Total / mat1Total;
 
-    quantityCalculationControllers.text =
-        calculationQuantity.toStringAsFixed(2);
+    terraceCalculationControllers.text = calculationQuantity.toStringAsFixed(2);
   }
 
   void _updateLaborHours() {
@@ -245,8 +246,7 @@ class _TerraceItemScreenState extends State<TerraceItemScreen> {
       material2Controllers[i].text = widget.material2[i].toStringAsFixed(2);
       totalPriceControllers[i].text = widget.totalPrice[i].toStringAsFixed(2);
     }
-    quantityCalculationControllers.text =
-        calculationQuantity.toStringAsFixed(2);
+    terraceCalculationControllers.text = calculationQuantity.toStringAsFixed(2);
     recalculateValues();
   }
 
@@ -311,60 +311,6 @@ class _TerraceItemScreenState extends State<TerraceItemScreen> {
 
   @override
   Widget build(BuildContext context) {
-    List<DataColumn> calculationColumns = calculationColumnsEng;
-
-    List<DataRow> calculationRows = [
-      DataRow(
-        cells: [
-          dataCellDoSingle(quantityCalculationControllers, (value) {
-            calculationQuantity = double.parse(value);
-            for (int i = 0; i < widget.description.length; i++) {
-              widget.laborHours2[i] = calculateWorkHours2(
-                  i, emptyCustomList, widget.laborHours1, calculationQuantity);
-              laborHours2Controllers[i].text = calculateWorkHours2(i,
-                      emptyCustomList, widget.laborHours1, calculationQuantity)
-                  .toStringAsFixed(2);
-
-              widget.laborCost[i] = calculateJobCost(
-                i,
-                widget.laborHours1,
-                hourlyRate,
-              );
-              laborCostControllers[i].text = calculateJobCost(
-                i,
-                widget.laborHours1,
-                hourlyRate,
-              ).toStringAsFixed(2);
-              widget.laborCost[i] = calculateJobCost(
-                i,
-                widget.laborHours2,
-                hourlyRate,
-              );
-              laborCostControllers[i].text = calculateJobCost(
-                i,
-                widget.laborHours2,
-                hourlyRate,
-              ).toStringAsFixed(2);
-              // Recalculate and update the material 2 when quantity changes
-              widget.material2[i] = calculateMaterialCost(
-                  i, widget.material1, calculationQuantity, emptyCustomList);
-              material2Controllers[i].text = calculateMaterialCost(
-                      i, widget.material1, calculationQuantity, emptyCustomList)
-                  .toStringAsFixed(2);
-              // Recalculate and update the total price when quantity changes
-              widget.totalPrice[i] = calculateTotalPrice(
-                  i, widget.laborCost, widget.material1, calculationQuantity);
-              totalPriceControllers[i].text = calculateTotalPrice(i,
-                      widget.laborCost, widget.material1, calculationQuantity)
-                  .toStringAsFixed(2);
-              //Rebuild the data table
-              rebuildDataTable();
-            }
-          }, Color.fromARGB(255, 218, 128, 122), false, 100),
-        ],
-      ),
-    ];
-
     List<DataColumn> columns = [
       createDataColumn("Description", 98, () {}),
       createDataColumn("Unit", 55, () {}),
@@ -709,15 +655,6 @@ class _TerraceItemScreenState extends State<TerraceItemScreen> {
                       );
                     });
                   }),
-              SingleChildScrollView(
-                scrollDirection: Axis.horizontal,
-                child: DataTable(
-                  dataRowMaxHeight: double.infinity,
-                  dataRowMinHeight: 60,
-                  columns: calculationColumns, // Define your columns here
-                  rows: calculationRows,
-                ),
-              )
             ],
           ),
         ),
--- lib/pages/shared/item_screens/terrace_screen.dart
+++ lib/pages/shared/item_screens/terrace_screen.dart
@@ -1,6 +1,7 @@
 import 'package:cost_calculator/data/data.dart';
 import 'package:cost_calculator/data/norw_data.dart';
 import 'package:cost_calculator/items/terrace_item.dart';
+import 'package:cost_calculator/pages/item_screen/terrace_item_screen.dart';
 import 'package:cost_calculator/pages/shared/home_page.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
@@ -43,53 +44,93 @@ class _TerraceScreenState extends State<TerraceScreen> {
         ],
         title: const Text('Bygg Kalk'),
       ),
-      body: languageEnglish == true
-          ? GridView.count(
-              padding: const EdgeInsets.all(25),
-              children: terraceData
-                  .map(
-                    (catData) => TerraceItem(
-                      catData.name,
-                      catData.description,
-                      catData.unit,
-                      catData.quantity,
-                      catData.laborHours1,
-                      catData.laborHours2,
-                      catData.laborCost,
-                      catData.material,
-                      catData.materials,
-                      catData.totalPrice,
-                      catData.color,
-                    ),
-                  )
-                  .toList(),
-              crossAxisCount: 1,
-              mainAxisSpacing: 20,
-              childAspectRatio: 7 / 2,
-            )
-          : GridView.count(
-              padding: const EdgeInsets.all(25),
-              children: norwTerraceData
-                  .map(
-                    (catData) => TerraceItem(
-                      catData.name,
-                      catData.description,
-                      catData.unit,
-                      catData.quantity,
-                      catData.laborHours1,
-                      catData.laborHours2,
-                      catData.laborCost,
-                      catData.material,
-                      catData.materials,
-                      catData.totalPrice,
-                      catData.color,
-                    ),
-                  )
-                  .toList(),
-              crossAxisCount: 1,
-              mainAxisSpacing: 20,
-              childAspectRatio: 7 / 2,
-            ),
+      body: GridView.count(
+        padding: const EdgeInsets.all(25),
+        children: languageEnglish == true
+            ? terraceData.map(
+                (catData) {
+                  return Row(
+                    children: [
+                      Expanded(
+                        child: TerraceItem(
+                          catData.name,
+                          catData.description,
+                          catData.unit,
+                          catData.quantity,
+                          catData.laborHours1,
+                          catData.laborHours2,
+                          catData.laborCost,
+                          catData.material,
+                          catData.materials,
+                          catData.totalPrice,
+                          catData.color,
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8),
+                        child: Container(
+                          width: 100,
+                          height: double.infinity,
+                          child: Center(
+                            child: TextField(
+                              controller: terraceCalculationControllers,
+                              onChanged: (value) {
+                                setState(() {
+                                  terraceCalculationControllers.text = value;
+                                });
+                              },
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  );
+                },
+              ).toList()
+            : norwTerraceData.map(
+                (catData) {
+                  return Row(
+                    children: [
+                      Expanded(
+                        child: TerraceItem(
+                          catData.name,
+                          catData.description,
+                          catData.unit,
+                          catData.quantity,
+                          catData.laborHours1,
+                          catData.laborHours2,
+                          catData.laborCost,
+                          catData.material,
+                          catData.materials,
+                          catData.totalPrice,
+                          catData.color,
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8),
+                        child: Container(
+                          width: 100,
+                          height: double.infinity,
+                          child: Center(
+                            child: TextField(
+                              controller: terraceCalculationControllers,
+                              onChanged: (value) {
+                                setState(() {
+                                  terraceCalculationControllers.text = value;
+                                });
+                              },
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  );
+                },
+              ).toList(),
+        crossAxisCount: 1,
+        mainAxisSpacing: 20,
+        childAspectRatio: 7 / 2,
+      ),
     );
   }
 }
