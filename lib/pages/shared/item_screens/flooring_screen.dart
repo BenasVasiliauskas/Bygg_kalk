e131d2bbd59e2d0503ef75aa306eda3b1980919f
Benas Vasiliauskas <benasvasi@gmail.com>
2024-06-30 10:59:07 +0300

    moved calc quantity outside deck screen

--- lib/pages/shared/item_screens/flooring_screen.dart
+++ lib/pages/shared/item_screens/flooring_screen.dart
@@ -1,6 +1,7 @@
 import 'package:cost_calculator/data/data.dart';
 import 'package:cost_calculator/data/norw_data.dart';
 import 'package:cost_calculator/items/flooring_item.dart';
+import 'package:cost_calculator/pages/item_screen/flooring_item_screen.dart';
 import 'package:cost_calculator/pages/shared/home_page.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
@@ -43,53 +44,93 @@ class _FlooringSectionsState extends State<FlooringScreen> {
         ],
         title: const Text('Bygg Kalk'),
       ),
-      body: languageEnglish == true
-          ? GridView.count(
-              padding: const EdgeInsets.all(25),
-              children: flooringData
-                  .map(
-                    (catData) => FlooringItem(
-                      catData.name,
-                      catData.description,
-                      catData.unit,
-                      catData.quantity,
-                      catData.laborHours1,
-                      catData.laborHours2,
-                      catData.laborCost,
-                      catData.material,
-                      catData.materials,
-                      catData.totalPrice,
-                      catData.color,
-                    ),
-                  )
-                  .toList(),
-              crossAxisCount: 1,
-              mainAxisSpacing: 20,
-              childAspectRatio: 7 / 2,
-            )
-          : GridView.count(
-              padding: const EdgeInsets.all(25),
-              children: norwFlooringData
-                  .map(
-                    (catData) => FlooringItem(
-                      catData.name,
-                      catData.description,
-                      catData.unit,
-                      catData.quantity,
-                      catData.laborHours1,
-                      catData.laborHours2,
-                      catData.laborCost,
-                      catData.material,
-                      catData.materials,
-                      catData.totalPrice,
-                      catData.color,
-                    ),
-                  )
-                  .toList(),
-              crossAxisCount: 1,
-              mainAxisSpacing: 20,
-              childAspectRatio: 7 / 2,
-            ),
+      body: GridView.count(
+        padding: const EdgeInsets.all(25),
+        children: languageEnglish == true
+            ? flooringData.map(
+                (catData) {
+                  return Row(
+                    children: [
+                      Expanded(
+                        child: FlooringItem(
+                          catData.name,
+                          catData.description,
+                          catData.unit,
+                          catData.quantity,
+                          catData.laborHours1,
+                          catData.laborHours2,
+                          catData.laborCost,
+                          catData.material,
+                          catData.materials,
+                          catData.totalPrice,
+                          catData.color,
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8),
+                        child: Container(
+                          width: 100,
+                          height: double.infinity,
+                          child: Center(
+                            child: TextField(
+                              controller: flooringCalculationControllers,
+                              onChanged: (value) {
+                                setState(() {
+                                  flooringCalculationControllers.text = value;
+                                });
+                              },
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  );
+                },
+              ).toList()
+            : norwFlooringData.map(
+                (catData) {
+                  return Row(
+                    children: [
+                      Expanded(
+                        child: FlooringItem(
+                          catData.name,
+                          catData.description,
+                          catData.unit,
+                          catData.quantity,
+                          catData.laborHours1,
+                          catData.laborHours2,
+                          catData.laborCost,
+                          catData.material,
+                          catData.materials,
+                          catData.totalPrice,
+                          catData.color,
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8),
+                        child: Container(
+                          width: 100,
+                          height: double.infinity,
+                          child: Center(
+                            child: TextField(
+                              controller: flooringCalculationControllers,
+                              onChanged: (value) {
+                                setState(() {
+                                  flooringCalculationControllers.text = value;
+                                });
+                              },
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  );
+                },
+              ).toList(),
+        crossAxisCount: 1,
+        mainAxisSpacing: 20,
+        childAspectRatio: 7 / 2,
+      ),
     );
   }
 }
